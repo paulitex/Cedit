fb35bf99069ddb5c78ff3765b14d28e920675510
Paul Lambert <paul@matygo.com>
2010-01-15 11:33:12 -0500

    Added i18n support, switched to using SafeConfigParser from iniparse for better editing api, add ability to delete sections and properties

--- hgrc.py
+++ hgrc.py
@@ -2,21 +2,25 @@
 Command Line Interface for editing .hgrc / Mercurial.ini configuration files
 '''
 
-from iniparse import INIConfig
+from mercurial.i18n import _
 from mercurial import commands, hg, util
-import os
+from iniparse import SafeConfigParser
+from ConfigParser import NoOptionError, NoSectionError
+import os, StringIO
 
-_options = ['&modify', '&view', '&reload', '&write', '&quit', '&help']
-_help = """
+_options = ['&add', '&remove', '&view', 're&load', '&write', '&quit', '&help']
+_options = map(lambda opt: _(opt), _options)
+_help =_( """
 Mercurial configuration editor extension.
 Commands:
-m       modify your configuration
+a       add to or modify your configuration
+r       remove a section or property from your configuration
 v       view current configuration, including changes
-w       write file and exit
+w       write to file and exit
 q       quit, discarding changes
-r       (re)load a new configuration from disk (discarding changes)
+l       load a new configuration from disk (discarding changes)
 h       view this help screen
-"""
+""")
 
 
 def hgrccli(ui, repo, **opts):
@@ -26,7 +30,7 @@ def hgrccli(ui, repo, **opts):
 
 class hgconfig(object):
     _ui = None
-    _conf = ""
+    _conf = None
     _path = ""
     _paths = []
 
@@ -39,9 +43,10 @@ class hgconfig(object):
         while True:
             # self._conf.sections()
             # self._conf.add_section("peter piper")
-            index = self._ui.promptchoice("(m, v, r, w, q, h)>>>",
+            index = self._ui.promptchoice(_("(a, r, v, l, w, q, h)>>>"),
             _options, len(_options) - 1) # default to 'help'
             [lambda: self.modsection(),
+            lambda: self.delsection(),
             lambda: self.viewconf(),
             lambda: self.reloadconf(),
             lambda: self.writeconf(),
@@ -50,51 +55,81 @@ class hgconfig(object):
 
     def modsection(self):
         """Adds or modifies sections and properties to current configuration"""
-        sec = self._ui.prompt("Enter section name: ", "")
-        if sec not in self._conf._sections.keys():
-            self._conf._new_namespace(sec)
-        prop = self._ui.prompt("Enter property name: ", "")
+        sec = self._ui.prompt(_("Enter section name: "), "")
+        if sec not in self._conf.sections():
+            self._conf.add_section(sec)
+        prop = self._ui.prompt(_("Enter property name: "), "")
         try:
-            old_val = " (currently '" + self._conf[sec][prop] + "'): "
-        except KeyError:
+            old_val = _(" (currently '%d'): ") % self._conf.get(sec, prop)
+        except NoOptionError:
             old_val = ": "
-        val = self._ui.prompt("Enter property value" + old_val, "")
-        self._conf[sec][prop] = val
+        val = self._ui.prompt(_("Enter property value %s") % old_val)
+        self._conf.set(sec, prop, val)
+        
+    def delsection(self):
+        self._ui.status(_("Delete an entire (s)ection or a single (p)roperty?\n"))
+        index = self._ui.promptchoice("(s, p)>>>", 
+        [_('&section'), _('&property')])
+        if index:
+            sec = self._ui.prompt(_("Enter section name: "), "")
+            prop = self._ui.prompt(_("Enter property name: "), "")
+            try:
+                if prop not in self._conf.options(sec):
+                    self._ui.warn(_("Property '%s' not found\n") % prop)
+                    return
+                removed = self._conf.remove_option(sec, prop)
+                if removed:
+                    self._ui.status(_("Property removed\n"))
+                else:
+                    self._ui.warn(_("Unable to remove property '%s'\n") % prop)
+            except NoSectionError:
+                self._ui.warn(_("Section not found.\n"))
+        else:
+            sec = self._ui.prompt(_("Enter section name: "), "")
+            if sec not in self._conf.sections():
+                self._ui.warn(_("Section '%s' not found\n") % sec)
+                return
+            removed = self._conf.remove_section(sec)
+            if removed:
+                self._ui.status(_("Section removed\n"))
+            else:
+                self._ui.warn(_("Unable to remove section '%s'\n") % sec)
 
     def viewconf(self):
-        self._ui.status("\n%s\n" % str(self._conf))
+        sb = StringIO.StringIO()
+        self._conf.write(sb)
+        self._ui.status("%s\n" % sb.getvalue())
 
     def reloadconf(self):
+        self._conf = SafeConfigParser()
         if len(self._paths) > 1:
-            self._ui.status("\nSelect configuration to edit:\n")
+            self._ui.status(_("\nSelect configuration to edit:\n"))
             for i in range(len(self._paths)):
                 print " " + str(i) + ".  " + self._paths[i]
             index = self._ui.promptchoice(">",
             map(lambda num: "&" + str(num), range(len(self._paths))),
             len(self._paths) - 1)
             self._path = self._paths[index]
-            self._conf = INIConfig(open(self._path))
         elif len(self._paths) == 1:
             self._path = self._paths[0]
-            self._conf = INIConfig(open(self._path))
         else:
             # This is a little silly, since without a valid config file
             # how could this extension be loaded? But for completeness...
             self._path = default = util.user_rcpath()[0]
-            index = self._ui.promptchoice("Unable to find configuration file."+
-            " Would you like to make one at %s?" % default,
-            ['&yes', '&no'], 'y')
+            msg = _("Unable to find configuration file."
+                    " Would you like to make one at %s?") % default
+            index = self._ui.promptchoice(msg, [_('&yes'), _('&no')], _('y'))
             if index == 1:
-                self._ui.status("No configuration to edit")
+                self._ui.status(_("No configuration to edit"))
                 exit(0)
-            self._conf = INIConfig(open(default, "a+"))
-        self._ui.status("Configuration loaded.\n")
+            open(default, "ab")
+        self._conf.read((self._path))
+        self._ui.status(_("Configuration loaded.\n"))
 
     def writeconf(self):
-        f = open(self._path, 'wb')
-        f.write(str(self._conf))
-        f.close()
-        self._ui.status("Configuration written to %s\n" % self._path)
+        with open(self._path, 'wb') as cfg:
+            self._conf.write(cfg)
+        self._ui.status(_("Configuration written to %s\n") % self._path)
         exit(0)
 
     def printhelp(self):
